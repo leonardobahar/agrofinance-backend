1ca6f06e49aa2f6d0bcb7571e77f7757be2fc984
george <georgewilliam187@gmail.com>
2020-12-20 11:21:43 +0700

    add even more validation that req.files is not undefined

--- app.js
+++ app.js
@@ -1577,8 +1577,8 @@ app.post("/api/transaksi/add",async(req,res)=> {
                 return res.send(error)
             }
 
-            console.log(req.files[0].filename)
-            console.log(req.files[1].filename)
+            console.log(req.files && req.files[0].filename)
+            console.log(req.files && req.files[1].filename)
             const transfer=new Transaksi(null,'NOW','NOW','NULL',req.body.is_rutin,'Pending',req.body.bon_sementara, req.body.id_rekening,
                 req.body.id_cabang_perusahaan,req.body.id_karyawan,'0',req.body.detail_transaksi,null,req.body.jumlah,req.body.id_kategori_transaksi,
                 req.files[0].filename,req.body.debit_credit,req.body.nomor_bukti_transaksi,
