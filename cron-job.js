5b2e52a18b7c8464653e0e90e713dfb6c3523477
Ferdinand <ferdinandgregorius@yahoo.com>
2021-02-08 22:46:24 +0700

    re-modified cron-job

--- cron-job.js
+++ cron-job.js
@@ -23,7 +23,40 @@ cron.schedule("1 0 * * *",function (){
 
     dao.retrieveTodayAndRutinTransaksi(date).then(transactionResult=>{
         for(let i=0; i<transactionResult.length; i++){
-
+            dao.addTransaksi(new Transaksi(
+                null,
+                transactionResult[i].t_tanggal_transaksi,
+                transactionResult[i].t_tanggal_modifikasi,
+                dateToday,
+                transactionResult[i].t_is_rutin,
+                transactionResult[i].t_status,
+                transactionResult[i].t_bon_sementara,
+                transactionResult[i].t_rekening_penanggung_utama,
+                transactionResult[i].t_id_cabang_perusahaan,
+                transactionResult[i].t_id_karyawan,
+                transactionResult[i].t_is_deleted,
+                transactionResult[i].detail_transaksi,
+                null,
+                transactionResult[i].td_jumlah,
+                transactionResult[i].td_id_kategori_transaksi,
+                transactionResult[i].td_bpu_attachment,
+                transactionResult[i].td_debit_credit,
+                transactionResult[i].td_nomor_bukti_transaksi,
+                transactionResult[i].td_file_bukti_transaksi,
+                transactionResult[i].skema_pembebanan_json,
+                transactionResult[i].td_is_deleted
+            )).then(result=>{
+                res.status(200).send({
+                    success:true,
+                    result:result
+                })
+            }).catch(error=>{
+                console.error(error)
+                res.status(500).send({
+                    success:false,
+                    error:SOMETHING_WENT_WRONG
+                })
+            })
         }
     }).catch(error=>{
         console.error(error)
